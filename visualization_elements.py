8b15b5f51b0142a252905eb413501feb49a14af4
Japneet Kaur <japneetkaur@Japneets-MacBook-Air.local>
2026-02-23 19:58:22 -0800

    added color configuration

--- visualization_elements.py
+++ visualization_elements.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle, Circle
 import numpy as np
 
 class VisualizationElements:
@@ -6,3 +7,15 @@ class VisualizationElements:
     def __init__(self):
         self.fig = None
         self.ax = None
+         self.colors = {
+            'left_half': 'lightblue',
+            'right_half': 'lightcoral',
+            'division_line': 'black',
+            'strip': 'yellow',
+            'delta_boundary': 'green',
+            'left_pair': 'blue',
+            'right_pair': 'red',
+            'overall_pair': 'purple',
+            'comparison_box': 'orange',
+            'text_bg': 'wheat'
+        }
